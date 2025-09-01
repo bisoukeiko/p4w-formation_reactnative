b109298ac47642a16b59022293cf6df7b8ca06e2
bisoukeiko <87bisou@gmail.com>
2025-09-01 23:29:35 +0200

    first commit: exercice 360

---
+++ app-cars/src/360/Cars.js
@@ -0,0 +1,24 @@
+// import React from 'react'
+
+const Cars = ({ nom, color, year}) => {
+
+    // console.log(props);
+
+    const colorInfo = color ? (<p>Couleur: { color }</p>) : (<p>Couleur: Néant</p>);
+
+    if(nom) {
+        return (
+            <div style={{backgroundColor: 'pink', width: '400px', padding: '10px', margin: '5px auto'} }>
+                <p>Marque: { nom }</p>
+                <p>Age: { year }</p>
+                { colorInfo }         
+            </div>
+        )
+    } else {
+        return null;
+    }
+
+
+}
+
+export default Cars
